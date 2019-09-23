2609b01768bcf1db8204dd10f99af3d199803b17
evanandrewson <evanandrewson@gmail.com>
2019-09-23 14:16:59 -0700

    updated schema with min, created test for full valid object

---
+++ __tests__/wildcat.test.js
@@ -0,0 +1,27 @@
+const Wildcat = require('../lib/models/wildcat');
+const mongoose = require('mongoose');
+
+describe('Wildcat model', () => {
+  it('validates model with all fields', () => {
+    const data = {
+      name: 'Jaguar',
+      size: 'Large',
+      weight: 165,
+      coloring: ['spots', 'brown', 'yellow'],
+      details: {
+        friendly: false,
+        funFact: 'Jaguars have no defined breeding season and will mate any time of year.'
+      }
+    };
+    const wildcat = new Wildcat(data);
+    const errors = wildcat.validateSync();
+    expect(errors).toBeUndefined();
+
+    const json = wildcat.toJSON();
+
+    expect(json).toEqual({
+      ...data,
+      _id: expect.any(Object)
+    });
+  });
+});
