196b4b0fc645f2d3a272f3580cebd77ce948f738
evanandrewson <evanandrewson@gmail.com>
2019-09-23 13:37:30 -0700

    set up configs and raw data

---
+++ data/cubs.js
@@ -0,0 +1,112 @@
+module.exports = [
+  {
+    name: 'Jaguar',
+    size: 'Large',
+    weight: 165,
+    coloring: ['spots', 'brown', 'yellow'],
+    details: {
+      friendly: false,
+      funFact: 'Jaguars have no defined breeding season and will mate any time of year.'
+    }
+  },
+  {
+    name: 'Leopard',
+    size: 'Medium',
+    weight: 60,
+    coloring: ['spots', 'brown', 'yellow'],
+    details: {
+      friendly: false,
+      funFact: 'Leopards are mostly nocturnal, hunting prey at night.'
+    }
+  },
+  {
+    name: 'Snow Leopard',
+    size: 'Large',
+    weight: 72,
+    coloring: ['spots', 'white', 'black'],
+    details: {
+      friendly: false,
+      funFact: 'Believe it or not, snow leopards can’t roar.'
+    }
+  },
+  {
+    name: 'Lion',
+    size: 'Extra-Large',
+    weight: 330,
+    coloring: ['brown'],
+    details: {
+      friendly: false,
+      funFact: 'A lion’s heels don’t touch the ground when it walks'
+    }
+  },
+  {
+    name: 'Cheetah',
+    size: 'Large',
+    weight: 103,
+    coloring: ['spots', 'brown', 'yellow'],
+    details: {
+      friendly: false,
+      funFact: 'a cheetah can reach 112km/h in just three seconds.'
+    }
+  },
+  {
+    name: 'Cougar',
+    size: 'Large',
+    weight: 142,
+    coloring: ['brown'],
+    details: {
+      friendly: false,
+      funFact: 'They actually hold the Guinness World Record for the animal with the highest number of names.'
+    }
+  },
+  {
+    name: 'Tiger',
+    size: 'Extra-Large',
+    weight: 405,
+    coloring: ['stripes', 'orange', 'black'],
+    details: {
+      friendly: false,
+      funFact: 'A tiger can consume up to 88 pounds of meat at one time.'
+    }
+  },
+  {
+    name: 'Sand Cat',
+    size: 'Small',
+    weight: 6,
+    coloring: ['stripes', 'brown', 'white'],
+    details: {
+      friendly: true,
+      funFact: 'Thanks to the furry paws, sand cats don\'t leave footprints in the sand.'
+    }
+  },
+  {
+    name: 'Rusty-Spotted Cat',
+    size: 'Small',
+    weight: 3,
+    coloring: ['spots', 'brown', 'white'],
+    details: {
+      friendly: true,
+      funFact: 'It has the nickname of the ‘hummingbird of the cat world’ because it is so agile.'
+    }
+  },
+  {
+    name: 'Serval',
+    size: 'Medium',
+    weight: 26,
+    coloring: ['spots', 'brown', 'yellow'],
+    details: {
+      friendly: true,
+      funFact: 'A servals\' hind legs are longer than its front ones'
+    }
+  },
+  {
+    name: 'Caracal',
+    size: 'Medium',
+    weight: 26,
+    coloring: ['brown'],
+    details: {
+      friendly: false,
+      funFact: 'Chinese emperors used caracals as gifts.'
+    }
+  },
+];
