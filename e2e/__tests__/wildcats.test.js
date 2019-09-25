5cf597e052ab372ba03c12b8cdb249da150a7772
evanandrewson <evanandrewson@gmail.com>
2019-09-25 15:09:10 -0700

    created tests for post route and get by id route

--- e2e/__tests__/wildcats.test.js
+++ e2e/__tests__/wildcats.test.js
@@ -15,5 +15,35 @@ describe('wildcats api', () => {
       friendly: false,
       funFact: 'Jaguars have no defined breeding season and will mate any time of year.'
     }
-  },
+  };
+
+  function postWildcat(obj) {
+    return request
+      .post('/api/wildcats')
+      .send(obj)
+      .expect(200)
+      .then(({ body }) => body);
+  }
+
+  it('posts an object to database', () => {
+    return postWildcat(jaguar)
+      .then(result => {
+        expect(result).toEqual({
+          _id: expect.any(String),
+          __v: 0,
+          ...jaguar
+        });
+      });
+  });
+
+  it('gets a wildcat by id', () => {
+    return postWildcat(jaguar)
+      .then(result => {
+        return request.get(`/api/wildcats/${result._id}`)
+          .expect(200)
+          .then(({ body }) => {
+            expect(body).toEqual(result);
+          });
+      });
+  });
 });
